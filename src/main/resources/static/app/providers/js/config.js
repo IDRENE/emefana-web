38ef325566504a848659594cb693f4e31a36131a
Iddy Magohe <iddyiam@gmail.com>
2015-06-11 00:14:06 -0400

    collaps panel

--- src/main/resources/static/app/providers/js/config.js
+++ src/main/resources/static/app/providers/js/config.js
@@ -3,5 +3,5 @@ var providerAppConfig = {
 	 * This might be desirable if headers don't work correctly in some
 	 * environments and is still secure when using https. */
 	useAuthTokenHeader: true,
-	authToken: "QEZJ22pp9OKN0C8yLmnG+MYyL4CgnRSbJJHn/TxD9Jf3mGqWcQLCwuQIIbY4SaWITRVS6CMjdb41nBkyXvxtEerHHA9v8cR36BnMyEj4M9U="
+	authToken: "QEZJ22pp9OKN0C8yLmnG+A0zIvFk/4CYwFXuQTdmv2yJkkj4L676bitt54D50WcWVLka2cesOqFRulnIhI8u6H/LM9JSzoLaKxoH3QG4Km0="
 };
