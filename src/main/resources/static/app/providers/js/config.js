ed0cfe88c58c49fe12910f5e5d3cf437931a8d89
Iddy Magohe <iddyiam@gmail.com>
2015-08-01 19:49:52 -0400

    small screen issue

--- src/main/resources/static/app/providers/js/config.js
+++ src/main/resources/static/app/providers/js/config.js
@@ -3,7 +3,7 @@ var providerAppConfig = {
 	 * This might be desirable if headers don't work correctly in some
 	 * environments and is still secure when using https. */
 	useAuthTokenHeader: true,
-	authToken: "QEZJ22pp9OKN0C8yLmnG+EvbCfq2Y/FAoJNKEtR0FDqCEA5unYdftKzutPbtUQcMIsDvBhR4baxCVWLulDU72ZFk8d0lsfAOi1Neyk2ahyM=",
-	//host: "52.5.231.243:8000"	
+	authToken: "QEZJ22pp9OKN0C8yLmnG+GLPTgs1SfHxDvIFEr1SEseSJr+EpJ8tGvJPUUldoqulFpcpJzZXVsmalTPoagDKH3UwL0OzLzHH3+NQb7+bDZM=",
+	//host: "52.6.85.129:8000"	"QEZJ22pp9OKN0C8yLmnG+GtHyqgkokqTojJ1IOyuP1r2ey/MQ8kwZn8TEKCIpwa1USD9sKWULOh9Ze0g28jJ0ipESGpRAog++E/9+69huks="
 	host:"10.0.0.21:8080"
 };
